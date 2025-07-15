6a1731dccd592d0616c39db52339389946d1345a
ezequiel-source <victor.arnault+github@gmail.com>
2025-07-14 21:01:07 -0300

    meus arquivos

---
+++ ACMEBicycle.java
@@ -0,0 +1,25 @@
+public class ACMEBicycle implements Bicycle_002{
+ int cadence = 0;
+ int speed = 0;
+ int gear = 1;
+
+ public void changeCadence(int newValue){
+  cadence = newValue;
+ }
+
+ public void changeGear(int newValue){
+  gear = newValue;
+ }
+
+ public void speedUp(int increment){
+  speed = speed + increment;
+ }
+
+ public void applyBrakes(int decrement){
+  speed = speed - decrement;
+ }
+
+ public void printStates(){
+  System.out.println("cadence: " + cadence + "speed: " + speed + "gear: "+ gear);
+ }
+}
---
+++ Bicycle_002.java
@@ -0,0 +1,9 @@
+public interface Bicycle_002{
+ void changeCadence(int newValue);
+ 
+ void changeGear(int newValue);
+
+ void speedUp(int increment);
+
+ void applyBrakes(int decrement);
+}
