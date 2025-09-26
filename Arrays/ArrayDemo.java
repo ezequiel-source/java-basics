888d3e4416c315ddfb70e5173f5314e7fab8de04
ezequiel-source <victor.arnault+github@gmail.com>
2025-09-25 21:21:51 -0300

    tipos de dados

---
+++ Arrays/ArrayDemo.java
@@ -0,0 +1,34 @@
+public class ArrayDemo{
+ public static void main(String... args){
+  //um array de inteiros
+  int[] anArray;
+
+ //aloca memória para 10 inteiros
+ anArray = new int[10];
+
+ //inicia o primeiro elemento
+ anArray[0] = 100;
+ //o segundo
+ anArray[1] = 200;
+ //e assim por diante
+ anArray[2] = 300;
+ anArray[3] = 400;
+ anArray[4] = 500;
+ anArray[5] = 600;
+ anArray[6] = 700;
+ anArray[7] = 800;
+ anArray[8] = 900;
+ anArray[9] = 1000;
+
+ System.out.println("Elemento no índice 0: " + anArray[0]);
+ System.out.println("Elemento no índice 1: " + anArray[1]);
+ System.out.println("Elemento no índice 2: " + anArray[2]);
+ System.out.println("Elemento no índice 3: " + anArray[3]);
+ System.out.println("Elemento no índice 4: " + anArray[4]);
+ System.out.println("Elemento no índice 5: " + anArray[5]);
+ System.out.println("Elemento no índice 6: " + anArray[6]);
+ System.out.println("Elemento no índice 7: " + anArray[7]);
+ System.out.println("Elemento no índice 8: " + anArray[8]);
+ System.out.println("Elemento no índice 9: " + anArray[9]);
+ }
+}
---
+++ Arrays/ArrayForDemo.java
@@ -0,0 +1,10 @@
+public class ArrayForDemo{
+ public static void main(String... args){
+  int[] anArray;
+  anArray = new int[10];
+  //preenhcer elementos do Array com o for
+  for(int i = 0; i<10; i++){
+      System.out.println("Os elementos do Arranjo são: " + anArray[i]);
+  }
+ }
+}
